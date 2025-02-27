d704f744cf97357dacfd216238e5296f1af1b291
mLeviGen <milkovalexandr2004@gmail.com>
2025-02-27 17:05:05 +0200

    updated for local path

--- app/handlers/menu_handler.py
+++ app/handlers/menu_handler.py
@@ -1,33 +1,27 @@
-from aiogram import Bot, types, Router
-from aiogram.filters import Command
-from aiogram.types import (
-    FSInputFile,
-    InputMediaPhoto,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-)
-from app.settings.config import *
-from app.funcs import get_username
+import os
 import logging
+from aiogram import Bot, types, Router
+from aiogram.types import FSInputFile, InputMediaPhoto, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
+from app.settings.config import CAPTION, BAD_ANSWER, bot
+from app.funcs import get_username
 
 router = Router()
-image_main = FSInputFile("app/images/menus/nugget.png")
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+image_main_path = os.path.join(BASE_DIR, "app", "images", "menus", "nugget.png")
+image_main = FSInputFile(image_main_path)
 
 def set_keyboard(user_id):
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [InlineKeyboardButton(text="🗺️ Карта", callback_data=f"show_map_{user_id}")],
-            [
-                InlineKeyboardButton(
-                    text="🚨 Тривога", callback_data=f"check_alert_{user_id}"
-                )
-            ],
+            [InlineKeyboardButton(text="🚨 Тривога", callback_data=f"check_alert_{user_id}")]
         ]
     )
 
-
-async def show_main_menu(bot, chat_id, user_id):
+async def show_main_menu(bot: Bot, chat_id: int, user_id: int):
     username = await get_username(user_id)
     try:
         await bot.send_photo(
@@ -39,16 +33,13 @@ async def show_main_menu(bot, chat_id, user_id):
     except Exception as e:
         logging.error(f"Помилка відправлення головного меню: {e}")
 
-
 @router.message(Command(commands=["menu"]))
 async def show_menu(message: types.Message, bot: Bot):
     await show_main_menu(bot, message.chat.id, message.from_user.id)
 
-
 @router.message(Command(commands=["start"]))
-async def show_menu(message: types.Message, bot: Bot):
+async def start_command(message: types.Message, bot: Bot):
     await show_main_menu(bot, message.chat.id, message.from_user.id)
-
 
 @router.callback_query(lambda c: c.data.startswith("back_to_menu_"))
 async def handle_back_to_menu(callback_query: types.CallbackQuery, bot: Bot):
